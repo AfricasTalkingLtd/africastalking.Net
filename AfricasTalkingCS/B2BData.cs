289424076e47a4190c787a371b09e223e4d04411
TheBeachmaster <otienoken@outlook.com>
2017-09-17 14:11:49 +0300

    Fixed an issue where Metadata was not part of the payload

--- AfricasTalkingCS/B2BData.cs
+++ AfricasTalkingCS/B2BData.cs
@@ -1,15 +1,18 @@
-﻿namespace AfricasTalkingCS
+﻿using System.Collections.Generic;
+using  Newtonsoft.Json;
+namespace AfricasTalkingCS
 {
     public class B2BData
     {
         public string username { get; set; }
         public string productName { get; set; }
-        public string providerChannel { get; set; }
+        public string provider { get; set; }
         public string currencyCode { get; set; }
         public decimal amount { get; set; }
         public string transferType { get; set; }
         public string destinationChannel { get; set; }
         public string destinationAccount { get; set; }
+        public dynamic metadata { get;  set; }
 
     }
 }
