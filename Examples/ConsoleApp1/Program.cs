a1fb2072960459a9046b3ed18a111beb9c2080f0
TheBeachmaster <otienoken@outlook.com>
2017-10-09 16:29:07 +0300

    Test emoji support :heavy_check_mark:

--- Examples/ConsoleApp1/Program.cs
+++ Examples/ConsoleApp1/Program.cs
@@ -7,21 +7,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            string username = "sandbox";
-            string apiKey = "bc203009d2b240e461c22d7a959ca4d752591d4553295d991e74824d599fc9b3";//"9f072129e0b34163eb8460a42f537bbb1564f50f8ca069fa779e27c64171f4e1";//"bc203009d2b240e461c22d7a959ca4d752591d4553295d991e74824d599fc9b3";
+            // Console.WriteLine("Hello World!");
+            var username = "YourUSERNAME";
+            var apiKey = "yourAPIKEY";
             string env = "sandbox";
-            string recep = "+254724587654";
-            string msg = "Super awesome message";
+            var recep = "+254724587654";
+            var msg = "Super awesome message ☻ 😁";
+               
 
-            AfricasTalkingGateway gateway = new AfricasTalkingGateway(username,apiKey,env);
+            var gateway = new AfricasTalkingGateway(username, apiKey, env);
 
             try
             {
                 dynamic res = gateway.SendMessage(recep, msg);
                 foreach (var re in res["SMSMessageData"]["Recipients"])
                 {
-                    Console.WriteLine((string)re["number"]+": ");
+                    Console.WriteLine((string)re["number"] + ": ");
                     Console.WriteLine((string)re["status"] + ": ");
                     Console.WriteLine((string)re["messageId"] + ": ");
                     Console.WriteLine((string)re["cost"] + ": ");
@@ -30,8 +31,8 @@ namespace ConsoleApp1
             catch (AfricasTalkingGatewayException exception)
             {
                 Console.WriteLine(exception);
-               // throw;
             }
+
             Console.ReadLine();
         }
     }
