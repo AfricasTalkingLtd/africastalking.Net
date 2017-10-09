619ac5a4f8db2315cda2048eac8dd45c2769d8b7
TheBeachmaster <otienoken@outlook.com>
2017-10-09 16:29:41 +0300

    Minor code refactorings

--- Examples/ConsoleApp2/Program.cs
+++ Examples/ConsoleApp2/Program.cs
@@ -6,30 +6,27 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            string username = "sandbox";
-            string environment = "sandbox";
-            string apiKey = "bc203009d2b240e461c22d7a959ca4d752591d4553295d991e74824d599fc9b3";
-            string productName = "coolproduct";
-            string phoneNumber = "+254724587654";
-            string currency = "KES";
+            var username = "sandbox";
+            var environment = "sandbox";
+            var apiKey = "bc203009d2b240e461c22d7a959ca4d752591d4553295d991e74824d599fc9b3";
+            var productName = "coolproduct";
+            var phoneNumber = "+254724587654";
+            var currency = "KES";
             int amount = 35700;
-            string channel = "mychannel";
+            var channel = "mychannel";
 
-            var gw = new AfricasTalkingGateway(username,apiKey,environment);
+            var gw = new AfricasTalkingGateway(username, apiKey, environment);
 
             try
             {
-               
                 var checkout = gw.Checkout(productName, phoneNumber, currency, amount, channel);
                 Console.WriteLine(checkout);
             }
             catch (AfricasTalkingGatewayException e)
             {
-                Console.WriteLine("We ran into problems: "+ e.Message);
-                
+                Console.WriteLine("We ran into problems: " + e.Message);
             }
             Console.ReadLine();
-
         }
     }
 }
