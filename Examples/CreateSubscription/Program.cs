4fc213e4837f5b39647afc2f62e4f08ae2c3325a
TheBeachmaster <otienoken@outlook.com>
2017-10-16 18:04:26 +0300

    Cleaned samples

--- Examples/CreateSubscription/Program.cs
+++ Examples/CreateSubscription/Program.cs
@@ -8,13 +8,13 @@ namespace CreateSubscription
         {
             Console.WriteLine("Hello World!");
             var username = "sandbox";
-            var apikey = "afd635a4f295dd936312836c0b944d55f2a836e8ff2b63987da5e717cd5ff745";
+            var apikey = "KEY";
             var env = "sandbox";
             var gateway = new AfricasTalkingGateway(username, apikey, env);
-            var shortCode = "44005";
-            var keyword = "coolguy";
-            var phoneNum = "+254724587654";
-            var token = "CkTkn_6d1d6387-ff82-45bd-9ceb-d125097d93c8";
+            var shortCode = "NNNNN";
+            var keyword = "keyword";
+            var phoneNum = "+254XXXXXXXXX";
+            var token = "Token";
             try
             {
                 var response = gateway.CreateSubscription(phoneNum, shortCode, keyword, token);
