3662e268454c530b7bfb79beca2b79db97802c7a
TheBeachmaster <otienoken@outlook.com>
2017-10-23 15:41:20 +0300

    Updated C2B sample to accommodate metadata

--- Examples/ConsoleApp2/Program.cs
+++ Examples/ConsoleApp2/Program.cs
@@ -2,24 +2,30 @@
 using  AfricasTalkingCS;
 namespace ConsoleApp2
 {
+    using System.Collections.Generic;
+
     class Program
     {
         static void Main(string[] args)
         {
             var username = "sandbox";
             var environment = "sandbox";
-            var apiKey = "bc203009d2b240e461c22d7a959ca4d752591d4553295d991e74824d599fc9b3";
+            var apiKey = "afd635a4f295dd936312836c0b944d55f2a836e8ff2b63987da5e717cd5ff745";
             var productName = "coolproduct";
             var phoneNumber = "+254724587654";
             var currency = "KES";
             int amount = 35700;
             var channel = "mychannel";
+            var metadata = new Dictionary<string, string>
+            {
+                { "reason", "broken car" }
+            };
 
             var gw = new AfricasTalkingGateway(username, apiKey, environment);
 
             try
             {
-                var checkout = gw.Checkout(productName, phoneNumber, currency, amount, channel);
+                var checkout = gw.Checkout(productName, phoneNumber, currency, amount, channel, metadata);
                 Console.WriteLine(checkout);
             }
             catch (AfricasTalkingGatewayException e)
