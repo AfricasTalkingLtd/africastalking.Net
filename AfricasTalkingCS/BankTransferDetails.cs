2bfae41cb64af64e54ca255c75e33d1ada6bb632
TheBeachmaster <otienoken@outlook.com>
2017-11-20 20:41:26 +0300

    List of params required for the transfer

---
+++ AfricasTalkingCS/BankTransferDetails.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfricasTalkingCS
+{
+    class BankTransferDetails
+    {
+        [JsonProperty("bankAccount")]
+        public List<BankAccountDetails> BankAccount { get; set; }
+        [JsonProperty("currencyCode")]
+        public string CurrencyCode { get; set; }
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
+        [JsonProperty("narration")]
+        public string Narration { get; set; }
+        [JsonProperty("metadata")]
+        public Dictionary<string,string>  Metadata { get; set; }
+        [JsonProperty("username")]
+        public string Username { get; set; }
+        public BankTransferDetails()
+        {
+            BankAccount = new List<BankAccountDetails>();
+        }
+        public void AddMetadata (string key, string value)
+        {
+            Metadata.Add(key, value);
+        }
+        public string ToJson()
+        {
+            var json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+        
+    }
+}
