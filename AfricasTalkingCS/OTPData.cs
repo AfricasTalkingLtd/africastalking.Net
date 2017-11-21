88fc64566c3309152e50f0aff79c2410863d38bf
TheBeachmaster <otienoken@outlook.com>
2017-11-21 11:05:58 +0300

    Added OTP params

---
+++ AfricasTalkingCS/OTPData.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfricasTalkingCS
+{
+    class OTPData
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+        [JsonProperty("transactionId")]
+        public string TransactionID { get; set; }
+        [JsonProperty("otp")]
+        public string OTP { get; set; }
+    }
+}
