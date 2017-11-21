31a05888526cc867416f209dac433023f5ca90ed
TheBeachmaster <otienoken@outlook.com>
2017-11-21 12:59:51 +0300

    Defined fileds required for OTP Validation for Cards

---
+++ AfricasTalkingCS/CardOTPData.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace AfricasTalkingCS
+{
+    public class CardOTPData
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+        [JsonProperty("transactionId")]
+        public string TransactionID { get; set; }
+        [JsonProperty("otp")]
+        public string OTP { get; set; }
+    }
+}
