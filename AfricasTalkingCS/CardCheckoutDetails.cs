05790b17c9298cf1773d004823488b9425586176
TheBeachmaster <otienoken@outlook.com>
2017-11-21 13:01:19 +0300

    Card Checkout Details

---
+++ AfricasTalkingCS/CardCheckoutDetails.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace AfricasTalkingCS
+{
+    public class CardCheckoutDetails
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+        [JsonProperty("productName")]
+        public string ProductName { get; set; }
+        [JsonProperty("paymentCard")]
+        public CardDetails PaymentCard { get; set; }
+        [JsonProperty("currencyCode")]
+        public string CurrencyCode { get; set; }
+        [JsonProperty("narration")]
+        public string Narration { get; set; }
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
+        [JsonProperty("metadata")]
+        public Dictionary<string, string> Metadata { get; set; }
+        public void AddMetadata(string key, string value)
+        {
+            Metadata.Add(key, value);
+        }
+        public string ToJson()
+        {
+            var json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+    }
+}
