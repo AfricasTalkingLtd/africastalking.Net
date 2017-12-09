397f72eb11e956ad43de0670acbb00f2efa965b2
TheBeachmaster <otienoken@outlook.com>
2017-12-09 21:33:53 +0300

    Added logic to initiate and process Card payments

---
+++ AfricasTalkingCS/CardDetailsConverter.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CardDetailsConverter.cs" company="Africa's Talking">
+//   2017
+// </copyright>
+// <summary>
+//   The converter.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AfricasTalkingCS
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// The converter.
+    /// </summary>
+    public static class CardDetailsConverter
+    {
+        /// <summary>
+        /// The settings.
+        /// </summary>
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+                                                                     {
+                                                                         MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+                                                                         DateParseHandling = DateParseHandling.None,
+                                                                     };
+    }
+}
