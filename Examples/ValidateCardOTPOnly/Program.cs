4a16ebb5d0f3f8abf90d946bc74d1e3d3316302d
TheBeachmaster <otienoken@outlook.com>
2017-12-10 02:19:54 +0300

    Modified OTP example

--- Examples/ValidateCardOTPOnly/Program.cs
+++ Examples/ValidateCardOTPOnly/Program.cs
@@ -12,7 +12,7 @@
             const string Username = "sandbox";
             const string Otp = "1234";
             const string ApiKey = "afd635a4f295dd936312836c0b944d55f2a836e8ff2b63987da5e717cd5ff745";
-            const string TransactionId = "ATPid_55bb5be3130b1a282143257f0f15f1cd";
+            const string TransactionId = "ATPid_66c6928f418e53a310c550511986d5c0";
             const string Env = "sandbox";
             var gateway = new AfricasTalkingGateway(Username, ApiKey, Env);
             try
