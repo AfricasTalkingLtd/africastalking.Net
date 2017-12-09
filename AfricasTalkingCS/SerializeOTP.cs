d4f1a8188d412f75387ae21be6aa9170a217a326
TheBeachmaster <otienoken@outlook.com>
2017-12-09 20:26:02 +0300

    Added Ability to validate card Charge Requests

---
+++ AfricasTalkingCS/SerializeOTP.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace AfricasTalkingCS
+{
+    public static class SerializeOTP
+    {
+        public static string OTPToJson(this OTP self) => JsonConvert.SerializeObject(self, OTPConverter.Settings);
+    }
+}
