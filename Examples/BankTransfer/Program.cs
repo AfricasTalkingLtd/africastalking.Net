3335707825089e4600eebd8cb70c08b39a711280
TheBeachmaster <otienoken@outlook.com>
2017-12-10 22:22:58 +0300

    Added Bank Transfer sample

---
+++ Examples/BankTransfer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankTransfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
