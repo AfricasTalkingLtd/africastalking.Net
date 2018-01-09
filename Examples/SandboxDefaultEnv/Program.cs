9823f806b7868b30f7747f1ca865902433789845
TheBeachmaster <otienoken@outlook.com>
2018-01-09 19:31:25 +0300

    Add sample that illustrates use of default auth for sandbox users without need to declare ENV

---
+++ Examples/SandboxDefaultEnv/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AfricasTalkingCS;
+namespace SandboxDefaultEnv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string username = "sandbox";
+            const string apikey = "mydopeSandboxKey";
+            const string phoneNum = "+254724587654";
+            const string message = "I have been Authe'd by default";
+            var gateway = new AfricasTalkingGateway(username, apikey);
+            Console.WriteLine("Hello World! Testing Default Auth");
+            try
+            {
+                var sms = gateway.SendMessage(phoneNum, message);
+                Console.WriteLine(sms);
+            }
+            catch (AfricasTalkingGatewayException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
