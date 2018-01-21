222bf1c9ca89814614c2313c7cd4fc8f6cbedc30
TheBeachmaster <otienoken@outlook.com>
2018-01-21 21:23:28 +0300

    Test voice sample

--- Examples/Voice.MakeCall/Program.cs
+++ Examples/Voice.MakeCall/Program.cs
@@ -8,11 +8,10 @@ namespace Voice.MakeCall
         {
             Console.WriteLine("Hello World!");
             var username = "sandbox";
-            var apiKey = "Key";
-            var env = "sandbox";
+            var apiKey = "afd635a4f295dd936312836c0b944d55f2a836e8ff2b63987da5e717cd5ff745";
             var from = "+254724545678";
-            var to = "+254724587654";
-            var gateway = new AfricasTalkingGateway(username, apiKey, env);
+            var to = "+254725587654";
+            var gateway = new AfricasTalkingGateway(username, apiKey);
 
             try
             {
