0ffdfc7c438287887b722812cd9bc060a87bcf0d
TheBeachmaster <otienoken@outlook.com>
2018-03-12 16:41:52 +0300

    Refactor Airtime Services

---
+++ AfricasTalkingSDK/AfricasTalkingSDK/Airtime/AirtimeEntry.cs
@@ -0,0 +1,15 @@
+﻿namespace AfricasTalkingSDK.Airtime
+{
+    partial class AirtimeResponse
+    {
+        public class AirtimeEntry
+        {
+            public string errorMessage;
+            public string phoneNumber;
+            public string amount;
+            public string discount;
+            public string status;
+            public string requestId;
+        }
+    }
+}
---
+++ AfricasTalkingSDK/AfricasTalkingSDK/Airtime/AirtimeResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AfricasTalkingSDK.Airtime
+{
+    partial class AirtimeResponse
+    {
+        public int numSent;
+        public string totalAmount;
+        public string totalDiscount;
+        public string errorMessage;
+        public List<AirtimeEntry> responses;
+    }
+}
