867723cc8aeacdbf5bdfcb37bcd0cadc19f0e64a
TheBeachmaster <otienoken@outlook.com>
2018-03-13 20:58:48 +0300

    Add test for Fetch messages

--- AfricasTalkingSDK/UnitTestAfricasTalkingSDK/UnitTestSMSService.cs
+++ AfricasTalkingSDK/UnitTestAfricasTalkingSDK/UnitTestSMSService.cs
@@ -70,6 +70,56 @@ namespace UnitTestAfricasTalkingSDK
             var assertSenderIdResponse = response.Contains("Success");
             Assert.IsTrue(assertSenderIdResponse);
         }
+
+        [TestMethod]
+        public void TestSenderIdEnqueue()
+        {
+            const string message = "Hello From SenderID Enqueue";
+            const string senderId = "Coolguy";
+            var response = _sms.Send(message, _phoneList, senderId, true);
+            var assertSenderIdResponseEnqueue = response.Contains("Success");
+            Assert.IsTrue(assertSenderIdResponseEnqueue);
+        }
+
+        
+        [TestMethod]
+        public void TestShortCodeEnqueue()
+        {
+            const string message = "Hello From ShortCode Enqueue";
+            const string shortCode = "44005";
+            var response = _sms.Send(message, _phoneList, shortCode, true);
+            var assertShortCodeResponseEnqueue = response.Contains("Success");
+            Assert.IsTrue(assertShortCodeResponseEnqueue);
+        }
     }
 
+    [TestClass]
+    public class TestSmsFeatures
+    {
+        static string username = "sandbox";
+        static string apikey = "afd635a4f295dd936312836c0b944d55f2a836e8ff2b63987da5e717cd5ff745";
+
+        private readonly SmsService _sms = new SmsService(username, apikey);
+       
+        [TestMethod]
+        public void TestFetchMessage()
+        {
+            const int receivedId = 0;
+            var response = _sms.FetchMessages(lastReceivedId:receivedId);
+            bool assertFetchSuccess = response.Length > 0;
+            Assert.IsFalse(!assertFetchSuccess);
+        }
+
+        [TestMethod]
+        public void TestFetchSubscription()
+        {
+            const int receivedId = 0;
+            const string shortCode = "44005";
+            const string keyword = "Coolguy";
+            var response = _sms.FetchSubscriptions(shortCode, keyword, receivedId);
+            bool assertSubscriptionFetch = response.Length > 0;
+            Assert.IsTrue(assertSubscriptionFetch);
+        }
+    }
+    
 }
