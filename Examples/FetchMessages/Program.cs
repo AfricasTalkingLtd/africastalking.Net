107ca5f7aa55448e0f19c46427841b525411c09b
TheBeachmaster <otienoken@outlook.com>
2018-09-28 20:08:46 +0300

    Patch #27 Fix fetch messages example

--- Examples/FetchMessages/Program.cs
+++ Examples/FetchMessages/Program.cs
@@ -9,12 +9,11 @@ namespace FetchMessages
             Console.WriteLine("Hello World!");
             const string Username = "sandbox";
             const string ApiKey = "afd635a4f295dd936312836c0b944d55f2a836e8ff2b63987da5e717cd5ff745";
-            const string Env = "sandbox";
             var gateway = new AfricasTalkingGateway(Username,ApiKey);
             int msgId = 0;
             try
             {
-                var res = gateway.FetchMessages(0);
+                var res = gateway.FetchMessages(msgId);
                 Console.WriteLine(res);
             }
             catch (AfricasTalkingGatewayException e)
