f4179a65406d7292783638f57213915157b57420
TheBeachmaster <otienoken@outlook.com>
2018-12-06 12:34:22 +0300

    master-patch_01 Define response object classes

---
+++ AfricasTalkingCS/BankCheckoutResponse.cs
@@ -0,0 +1,51 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BankCheckoutResponse.cs" company="Africa's Talking">
+//   2018
+// </copyright>
+// <summary>
+//   Defines the BankCheckoutResponse Class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AfricasTalkingCS
+{
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// The BankCheckout response result.
+    /// </summary>
+    public class BankCheckoutResponse
+    {
+        /// <summary>
+        /// Gets or sets the Transaction ID.
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Transaction ID.
+        /// </summary>
+        [JsonProperty("transactionId")]
+        public string TransactionId { get; set; }
+
+        /// <summary>
+        /// Sets/ Gets status.
+        /// </summary>
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        /// <summary>
+        /// The to string.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public override string ToString()
+        {
+            var result = JsonConvert.SerializeObject(this);
+            return result;
+        }
+    }
+}
---
+++ AfricasTalkingCS/C2BDataResults.cs
@@ -0,0 +1,57 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="C2BDataResults.cs" company="Africa's Talking">
+//   2018
+// </copyright>
+// <summary>
+//   Defines the C2BDataResults Class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AfricasTalkingCS
+{
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// The C2B response result.
+    /// </summary>
+    public class C2BDataResults
+    {
+        /// <summary>
+        /// Gets or sets the Provider Channel.
+        /// </summary>
+        [JsonProperty("providerChannel")]
+        public string ProviderChannel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Transaction ID.
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Transaction ID.
+        /// </summary>
+        [JsonProperty("transactionId")]
+        public string TransactionId { get; set; }
+
+        /// <summary>
+        /// Sets/ Gets status.
+        /// </summary>
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        /// <summary>
+        /// The to string.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public override string ToString()
+        {
+            var result = JsonConvert.SerializeObject(this);
+            return result;
+        }
+    }
+}
