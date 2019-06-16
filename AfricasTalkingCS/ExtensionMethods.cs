7188c8259e3f42e6e48f149cb7a01215657f6f14
Timothy Macharia <timothy.macharia@outlook.com>
2019-06-16 22:35:11 +0300

    refactors code & applies code re-use

--- AfricasTalkingCS/ExtensionMethods.cs
+++ AfricasTalkingCS/ExtensionMethods.cs
@@ -1,6 +1,4 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
@@ -9,15 +7,37 @@ namespace AfricasTalkingCS
 {
     public static class ExtensionMethods
     {
+        public static bool IsValid(this string s) {
+            if (!string.IsNullOrWhiteSpace(s)) {
+                return s.Length > 0;
+            }
+            return false;
+        }
+
+        public static int ToInt(this HttpStatusCode code) => (int)code;
+
         public static string ToJson<T>(this T model)
         {
             if (model == null)
                 return string.Empty;
             return JsonConvert.SerializeObject(model);
         }
+
         public static TResult FromJsonTo<TResult>(this string json)
             => JsonConvert.DeserializeObject<TResult>(json);
 
-        public static int ToInt(this HttpStatusCode code) => (int)code;
+        public static IEnumerable<KeyValuePair<string,string>> CastToCollection(this IDictionary dictionary)
+        {
+            List<KeyValuePair<string, string>> list = new List<KeyValuePair<string, string>>();
+            if (dictionary == null)
+                return list;
+            IDictionaryEnumerator enumerator = dictionary.GetEnumerator();
+
+            while (enumerator.MoveNext()) {
+                var item = (DictionaryEntry)enumerator.Current;
+                list.Add(new KeyValuePair<string, string>(item.Key.ToString(), item.Value.ToString()));
+            }
+            return list;
+        }
     }
 }
---
+++ AfricasTalkingCS/HttpVerb.cs
@@ -0,0 +1,17 @@
+﻿namespace AfricasTalkingCS
+{
+    /// <summary>
+    /// Represents types of HTTP requests that can be sent.
+    /// </summary>
+    public enum HttpVerb
+    {
+        /// <summary>
+        /// Http GET Request.
+        /// </summary>
+        GET,
+        /// <summary>
+        /// Http POST Request.
+        /// </summary>
+        POST
+    }
+}
