5cace41983e0dc85e0628e9a8608b17ea89b423a
Kennedy Otieno <otienoken@outlook.com>
2020-12-03 11:11:59 +0300

    Fix Fix Premium SMS sending issue by renaming bulkSmsMode to bulkSMSMode

--- Examples/SendPremiumSMS/Program.cs
+++ Examples/SendPremiumSMS/Program.cs
@@ -9,17 +9,17 @@ namespace SendPremiumSMS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var username = "sandbox";
-            var apikey = "KEY";
-            var env = "sandbox";
-            var gateway = new AfricasTalkingGateway(username, apikey, env);
-            var opts = new Hashtable { ["keyword"] = "mykeyword" }; // ....
-            var from = "NNNNN";
-            var to = "+2547XXXXX";
+            var username = "USERNAME";
+            var apikey = "APIKEY";
+            // var env = "sandbox";
+            var gateway = new AfricasTalkingGateway(username, apikey/*, env*/);
+            var opts = new Hashtable { ["keyword"] = "myKeyword", ["linkId"] = "NNNNNNN" }; // ....
+            var from = "NNNNNNNNNN";
+            var to = "+NNNNNNNNNN";
             var message = "Super Cool Message";
             try
             {
-                var res = gateway.SendMessage(to, message, from, 1, opts); // Set Bulk to true
+                var res = gateway.SendPremiumMessage(to, message, from, 0, opts); // Set Bulk to true
                 Console.WriteLine(res);
             }
             catch (AfricasTalkingGatewayException e)
